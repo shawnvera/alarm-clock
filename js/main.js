1c3a5401efecd09fb09cf4faf45da20ad0334510
Shawn Vera <shawnvera@gmail.com>
2023-06-01 17:58:31 +0000

    updated JS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
+function startTime() {
+    const today = new Date();
+    let hour = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds)
+document.getElementById('time').innerHTML = hour + ':' + minutes + ":" + seconds;
+setTimeout(startTime, 1000);
+
+
+}
+
+function checkTime() {
+    if (i < 10) { i = "0" + i };
+    return i;
+}
+
+
 let btn = document.getElementById("btn");
 let alarmTime = document.getElementById('alarm-time');
 
-btn.addEventListener('click', ()=> {
-    
+btn.addEventListener('click', () => {
+
     console.log(alarmTime);
 })
 
-function getAlarmTime () {
+function getAlarmTime() {
     document.getElementById('alarm-time');
     console.log(alarmTime);
 }
