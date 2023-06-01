252ccfc71d47fdbc79739670885c2c4869374754
Shawn Vera <shawnvera@gmail.com>
2023-06-01 16:26:39 +0000

    working on JS and index

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 let btn = document.getElementById("btn");
-body.appendChild(btn);
+let alarmTime = document.getElementById('alarm-time');
+
+btn.addEventListener('click', ()=> {
+    
+    console.log(alarmTime);
+})
+
+function getAlarmTime () {
+    document.getElementById('alarm-time');
+    console.log(alarmTime);
+}
 
 
 let time = new Date();
