cfe648297c0921346e5a34dbfe33c306cf8125eb
Shawn Vera <shawnvera@gmail.com>
2023-06-02 00:03:41 +0000

    updated JS and HTML

--- js/main.js
+++ js/main.js
@@ -1,19 +1,70 @@
-// initial variables
+// function to convert time to string value and adding 0 if under 10.
 
+const getTimeString = ({ hours, minutes, seconds, zone }) => {
+    if (minutes / 10 < 1) {
+        minutes = "0" + minutes;
+    }
+    if (seconds / 10 < 1) {
+        seconds = "0" + seconds;
+    }
+    return `${hours}:${minutes}:${seconds}:${zone}`;
+};
+
+// function to display current time on the screen.
+const renderTime = () => {
+    var currentTime = document.getElementById("clock");
+    const currentDate = new Date();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    var zone = hours >= 12 ? "PM" : "AM";
+    if (hours > 12) {
+        hours = hours % 12;
+    }
+    const timeString = getTimeString({ hours, minutes, seconds, zone});
+    currentTime.innerHTML = timeString;
+}
+
+// Update time every second\
+
+setInterval(renderTime, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// initial variables
+/*
 let timeRef = document.getElementById('time-display');
 let hourInput = document.getElementById('hour').value;
 let minuteInput = document.getElementById('minute');
 let activeAlarms = document.getElementById('active-alarms');
 let setAlarm = document.getElementById('set-alarm');
-let alarmsArray = [];
 
+
+const alarmsArray = [];
+let userAlarm;
 function alarmTimer () {
-    let userAlarm = document.getElementById('alarm').value;
-    document.getElementById('test').innerHTML = userAlarm;
-    alarmsArray.push(userAlarm);
+    userAlarm = document.getElementById('alarm').value;
+    document.getElementById('clock').innerHTML = userAlarm;
+    alarmsArray.push(userAlarm.value);
+    
+}
+for (let i = 0; i < alarmsArray.length; i++){
+console.log(alarmsArray[i]);
 }
 
-console.log(alarmsArray);
+console.log(userAlarm);
 
 let initialHour = 0, initialMinute = 0, alarmIndex = 0;
 
@@ -23,7 +74,6 @@ function checkTime(i) {
     if (i < 10) { i = "0" + i };
     return i;
 }
-
 
 function startTime() {
     const today = new Date();
@@ -36,9 +86,6 @@ function startTime() {
     setTimeout(startTime, 1000);
 }
 
-
-
-/*
 let btn = document.getElementById("btn");
 let alarmTime = document.getElementById('alarm-time');
 
@@ -51,7 +98,6 @@ function getAlarmTime() {
     document.getElementById('alarm-time');
     console.log(alarmTime);
 }
-
 
 let time = new Date();
 document.createElement('div')
