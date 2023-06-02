34b42457c85157f5435659972427dbb109fa3243
Shawn Vera <shawnvera@gmail.com>
2023-06-02 13:17:16 +0000

    updated JS and index

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // function to convert time to string value and adding 0 if under 10.
-
 const getTimeString = ({ hours, minutes, seconds, zone }) => {
   if (minutes / 10 < 1) {
     minutes = "0" + minutes;
@@ -23,83 +22,34 @@ const renderTime = () => {
   }
   const timeString = getTimeString({ hours, minutes, seconds, zone });
   currentTime.innerHTML = timeString;
-}
+};
 
-// Update time every second\
-
+// Update time every second
 setInterval(renderTime, 1000);
 
 const alarmsArray = [];
 let userAlarm;
-function alarmTimer() {
+function alarmTimer(e) {
   userAlarm = document.getElementById('alarm').value;
   document.getElementById('clock').innerHTML = userAlarm;
-  alarmsArray.push(userAlarm.value);
+  alarmsArray.push(userAlarm);
 
+  for (let i = 0; i < alarmsArray.length; i++) {
+    console.log(alarmsArray[i]);
+    let alarms = document.getElementById("active-alarms");
+    alarms.innerHTML = alarmsArray;
 }
-for (let i = 0; i < alarmsArray.length; i++) {
-  console.log(alarmsArray[i]);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// initial variables
-/*
-let timeRef = document.getElementById('time-display');
-let hourInput = document.getElementById('hour').value;
-let minuteInput = document.getElementById('minute');
-let activeAlarms = document.getElementById('active-alarms');
-let setAlarm = document.getElementById('set-alarm');
-
-
-
-console.log(userAlarm);
-
-let initialHour = 0, initialMinute = 0, alarmIndex = 0;
-
-// functions for clock
-
-function checkTime(i) {
-  if (i < 10) { i = "0" + i };
-  return i;
 }
 
-function startTime() {
-  const today = new Date();
-  let hour = today.getHours();
-  let mins = today.getMinutes();
-  let secs = today.getSeconds();
-  mins = checkTime (mins);
-  secs = checkTime(secs)
-  document.getElementById('clock').innerHTML = hour + ":" + mins + ":" + secs;
-  setTimeout(startTime, 1000);
-}
 
-let btn = document.getElementById("btn");
-let alarmTime = document.getElementById('alarm-time');
 
-btn.addEventListener('click', () => {
 
-  console.log(alarmTime);
-})
 
-function getAlarmTime() {
-  document.getElementById('alarm-time');
-  console.log(alarmTime);
-}
 
-let time = new Date();
-document.createElement('div')
-div.appendChild(time);
-*/
+
+
+
+
+
+
+
